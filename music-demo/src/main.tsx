923d5a61fecf793b92b90b2e7e84c8d91f35c9f2
Nikita Vyskubov <vysnik@gmail.com>
2025-12-29 12:28:19 +0300

    главный  App

--- music-demo/src/main.tsx
+++ music-demo/src/main.tsx
@@ -1,8 +1,8 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import { App } from './App.tsx'
-import { App } from './Study.tsx'
+import { App } from './App.tsx'
+// import { App } from './Study.tsx'
 
 const rootEl = document.getElementById('root');
 const reactRoot = createRoot(rootEl!);
